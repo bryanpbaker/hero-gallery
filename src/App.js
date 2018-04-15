cd9cdaca5045334f5eaea5d9088c3e0262052cd8
Bryan Baker <bryanpbaker@gmail.com>
2018-04-15 18:01:07 -0500

    added .env with keys and make test request to marvel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import "normalize.css";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    fetch(
+      "https://gateway.marvel.com/v1/public/characters?name=Wolverine&apikey=1425b32d20159156ffcf8bf2b690d736"
+    )
+      .then(res => res.json())
+      .then(response => console.log(response.data.results));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Marvel Hero Gallery</h1>
       </div>
     );
   }
